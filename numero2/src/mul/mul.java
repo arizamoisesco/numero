f5a651e37b110e7997e32511f8d8fa9b894bf0ea
elnosferatus <arizamoises@gmail.com>
2014-09-11 13:01:21 -0500

    Update mul.java

--- numero2/src/mul/mul.java
+++ numero2/src/mul/mul.java
@@ -4,5 +4,21 @@ public class mul {
 
     public static void main (string [] args){
 	
+	BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int numero1 = Integer.parseInt(br.readLine());
+        int numero2 = Integer.parseInt(br.readLine());
+        
+        try{ 
+         System.out.println("Introduce el primer número:");			
+         numero1 = Integer.parseInt(br.readLine());			
+         System.out.println("Introduce el segundo número:");
+         numero2 = Integer.parseInt(br.readLine());
+        } catch (IOException ioe){
+        }
+        int resultado = numero1*numero2;
+        System.out.println("La multiplicación es " + numero1 + " x " + numero2 + " = " + resultado);
+        
 	}
 }
