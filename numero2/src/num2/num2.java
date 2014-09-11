2cba0ad44b2ee14e7640f7ea61db1c4a140ab8d1
elnosferatus <>
2014-09-11 10:45:41 -0500

    ejercicio 1

---
+++ numero2/src/num2/num2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package num2;
+
+/**
+ *
+ * @author JOHAN
+ */
+public class num2 {
+    
+      public static void main (String [] args){
+        int sum = 0; 
+        int i;
+        for (i = 1 ; i <= 100 ; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
